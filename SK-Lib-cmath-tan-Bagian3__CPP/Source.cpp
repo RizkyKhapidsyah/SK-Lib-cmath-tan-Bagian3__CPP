fe09a322dc2ea88387fdd4363fe83b0071c238b9
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-05-23 13:10:11 +0700

    Add project files.

---
+++ SK-Lib-cmath-tan-Bagian3__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main(){
+	long int x = 6;
+	double result;
+
+	result = tan(x);
+	cout << "tan(x) = " << result;
+
+	_getch();
+	return 0;
+}
